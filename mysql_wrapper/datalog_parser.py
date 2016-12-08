6600e5389f7433e4b3dd3e024e73cb9bbdf3b83f
Alex Egg <eggie5@gmail.com>
2016-12-07 20:50:57 -0800

    merged deepthi's changes

--- mysql_wrapper/datalog_parser.py
+++ mysql_wrapper/datalog_parser.py
@@ -26,13 +26,13 @@ class DatalogParser(object):
   def build_string(self, distinct_part, select,from_table,where_equality,where_inequality,predicates_constraint_dict,groupby_vars,having_dict,sort_var,sort_type,limit_num):
       ## Adding distinct logic to select
       if (len(distinct_part)>0):
-        select_clause = "SELECT DISTINCT "+ ", ".join(x+" AS "+y+" " for x,y in select.iteritems())
+          select_clause = "SELECT DISTINCT "+ ", ".join(x+" AS "+y+" " for x,y in select.iteritems())
       else:
-        select_clause = "SELECT "+ ", ".join(x+" AS "+y+" " for x,y in select.iteritems())
+          select_clause = "SELECT "+ ", ".join(x+" AS "+y+" " for x,y in select.iteritems())
       from_clause = "FROM "+ from_table + " "
       where_clause =""
-      ## Where clause
 
+  ## Where clause
       if (len(where_equality)>0): ## DN added this
           for k,v in where_equality.iteritems():
               if type(v)==list:
@@ -61,25 +61,43 @@ class DatalogParser(object):
                   else:
                       predicates_constraint_dict[k] = v
                     
-        
-                    
-      for k,v in predicates_constraint_dict.iteritems():
-          if type(v[0]) == list:
-              x = where_inequality[k[:k.find('_')]]
-              temp = []
-              for i in range(len(v)):
-                  temp.append(" "+v[i][0]+" "+v[i][1])
-              if (len(where_clause)>0):
-                  where_clause = where_clause + " AND " +"("+ " OR ".join(x+z+" " for z in temp)+")"+" "
-              else:
-                  where_clause = "WHERE " +"("+ " OR ".join(x+z+" " for z in temp)+")"+" "
 
-          else:
-              x = where_inequality[k[:k.find('_')]]
-              if (len(where_clause)>0):
-                  where_clause = where_clause +" AND "+(x+" "+v[0]+" "+v[1]+" " )
+      for k,v in predicates_constraint_dict.iteritems():
+              if type(v[0]) == list:
+                  x = where_inequality[k[:k.find('_')]]
+                  temp = []
+                  for i in range(len(v)):
+                      temp.append(" "+v[i][0]+" "+v[i][1])
+                  if (len(where_clause)>0):
+                      where_clause = where_clause + " AND " +"("+ " OR ".join(x+z+" " for z in temp)+")"+" "
+                  else:
+                      where_clause = "WHERE " +"("+ " OR ".join(x+z+" " for z in temp)+")"+" "
               else:
-                  where_clause = "WHERE " + (x+" "+v[0]+" "+v[1]+" " )
+                  if len(re.findall('\+|\-|\*|/',k))>0:
+                      arith_split = re.split('\+|\-|\*|/',k)
+                      arith_symbols = re.findall('\+|\-|\*|/',k)
+                      x=[]
+                      for i in range(len(arith_split)-1):
+                          x.append(where_inequality[arith_split[i]])
+                      x.append(where_inequality[arith_split[(len(arith_split))-1][:arith_split[(len(arith_split))-1].find('_')]])
+                      c = []
+                      arith_symbols.append("")
+                      for i in range(len(arith_split)):
+                          c.append(x[i])
+                          c.append(arith_symbols[i])    
+                      arith_str = ''.join(str(elem) for elem in c)
+             
+                      if (len(where_clause)>0):
+                          where_clause = where_clause +" AND "+(arith_str+" "+v[0]+" "+v[1]+" " )
+                      else:
+                          where_clause = "WHERE " + (arith_str+" "+v[0]+" "+v[1]+" " )
+                  else:
+                      x = where_inequality[k[:k.find('_')]]
+                      if (len(where_clause)>0):
+                          where_clause = where_clause +" AND "+(x+" "+v[0]+" "+v[1]+" " )
+                      else:
+                          where_clause = "WHERE " + (x+" "+v[0]+" "+v[1]+" " )
+
 
   ## Group by            
       if len(groupby_vars) > 0:
@@ -106,6 +124,7 @@ class DatalogParser(object):
   
 
   def dl_parse(self, left, right):
+
     return_table = left[:left.find("(")]
     return_select = [c.strip() for c in left[left.find("(")+1:left.find(")")].split(',')]
 
@@ -141,7 +160,8 @@ class DatalogParser(object):
     ## DN Added - End
 
     predicates_constraint_all = query_predicates.replace(groupby_part,'').replace(sort_part,'')\
-                                .replace(limit_part,'').replace(distinct_part,'').replace(' ,','').strip().split(',')
+                                                .replace(limit_part,'').replace(' ,','').replace(distinct_part,'')\
+                                                .strip().split(',')
 
     predicates_constraint_dict = {}
     having_dict = {}
@@ -211,7 +231,6 @@ class DatalogParser(object):
         except KeyError:
             select_reversed = {v: k for k, v in select.iteritems()}
             groupby_vars = ', '.join([select_reversed[g] for g in groupby_var_list])
-
     ## DN added this
     # For sortby
     sort_var=[]
@@ -227,88 +246,88 @@ class DatalogParser(object):
     return [distinct_part,select,from_table,where_equality,where_inequality,predicates_constraint_dict,groupby_vars,having_dict,sort_var,sort_type,limit_num]
     
   def sql_generator(self, datalog_str):
-      global separator
-      separator = ":-"
-      global dummy
-      dummy = "_"
-      global allowed_comparisons
-      allowed_comparisons = '=|!=|>|<|<=|>='
+    global separator
+    separator = ":-"
+    global dummy
+    dummy = "_"
+    global allowed_comparisons
+    allowed_comparisons = '=|!=|>|<|<=|>='
 
-      or_queries=[datalog_str.split(separator)[0]]
-      right_all=[]
-      ## For OR query
-      if len(re.findall(separator,datalog_str))>1:
-          for i in (datalog_str.split(separator))[1:]:
-              or_queries.append(i.replace(datalog_str.split(separator)[0],""))
-              left = or_queries[0]
-              right_all = or_queries[1:]
-          output = []
-          for i in range(len(right_all)):
-              output.append(parse(left,right_all[i]))
-          equalities = [item[2] for item in output]
-          inequalities = [item[4] for item in output]
+    or_queries=[datalog_str.split(separator)[0]]
+    right_all=[]
+    ## For OR query
+    if len(re.findall(separator,datalog_str))>1:
+        for i in (datalog_str.split(separator))[1:]:
+            or_queries.append(i.replace(datalog_str.split(separator)[0],""))
+            left = or_queries[0]
+            right_all = or_queries[1:]
+        output = []
+        for i in range(len(right_all)):
+            output.append(self.dl_parse(left,right_all[i]))
+        equalities = [item[2] for item in output]
+        inequalities = [item[4] for item in output]
 
-          ## When there are no inequalities
-          if all(x == equalities[0] for x in equalities) and all(x == inequalities[0] for x in inequalities):
-              sql_query = build_string(output[0][0],output[0][1],output[0][2],output[0][3],output[0][4],output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
+        ## When there are no inequalities
+        if all(x == equalities[0] for x in equalities) and all(x == inequalities[0] for x in inequalities):
+            sql_query = self.build_string(output[0][0],output[0][1],output[0][2],output[0][3],output[0][4],output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
 
-          ## When there are different criteria in the inequalities => or in inequalities
-          elif all(x == equalities[0] for x in equalities) and ~all(x == inequalities[0] for x in inequalities):
-              all_inequalities = []
-              for i in range(len(output)):
-                  all_inequalities.append(output[i][4])
-              all_inequalities_new_dict={}
-              for k,v in [(key,d[key]) for d in all_inequalities for key in d]:
-                  if k not in all_inequalities_new_dict: 
-                          all_inequalities_new_dict[k]=[v]
-                  else: 
-                      all_inequalities_new_dict[k].append(v)
-              sql_query = build_string(output[0][0],output[0][1],output[0][2],output[0][3],all_inequalities_new_dict,output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
+        ## When there are different criteria in the inequalities => or in inequalities
+        elif all(x == equalities[0] for x in equalities) and ~all(x == inequalities[0] for x in inequalities):
+            all_inequalities = []
+            for i in range(len(output)):
+                all_inequalities.append(output[i][4])
+            all_inequalities_new_dict={}
+            for k,v in [(key,d[key]) for d in all_inequalities for key in d]:
+                if k not in all_inequalities_new_dict: 
+                        all_inequalities_new_dict[k]=[v]
+                else: 
+                    all_inequalities_new_dict[k].append(v)
+            sql_query = self.build_string(output[0][0],output[0][1],output[0][2],output[0][3],all_inequalities_new_dict,output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
 
 
-          ## When there are different criteria in the equalities => or in equalities
-          elif ~all(x == equalities[0] for x in equalities) and all(x == inequalities[0] for x in inequalities):
-              all_equalities = []
-              for i in range(len(output)):
-                  all_equalities.append(output[i][2])
-              all_equalities_new_dict={}
-              for k,v in [(key,d[key]) for d in all_equalities for key in d]:
-                  if k not in all_equalities_new_dict: 
-                          all_equalities_new_dict[k]=[v]
-                  else:
-                      all_equalities_new_dict[k].append(v)
-              sql_query = build_string(output[0][0],output[0][1],all_equalities_new_dict,output[0][3],output[0][4],output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
+        ## When there are different criteria in the equalities => or in equalities
+        elif ~all(x == equalities[0] for x in equalities) and all(x == inequalities[0] for x in inequalities):
+            all_equalities = []
+            for i in range(len(output)):
+                all_equalities.append(output[i][2])
+            all_equalities_new_dict={}
+            for k,v in [(key,d[key]) for d in all_equalities for key in d]:
+                if k not in all_equalities_new_dict: 
+                        all_equalities_new_dict[k]=[v]
+                else:
+                    all_equalities_new_dict[k].append(v)
+            sql_query = self.build_string(output[0][0],output[0][1],all_equalities_new_dict,output[0][3],output[0][4],output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
 
-          ## When there are different criteria in both equalities and inequalities => or in both equalities and inequalities
-          elif ~all(x == equalities[0] for x in equalities) and ~all(x == inequalities[0] for x in inequalities):
-              all_inequalities = []
-              for i in range(len(output)):
-                  all_inequalities.append(output[i][4])
-              all_inequalities_new_dict={}
-              for k,v in [(key,d[key]) for d in all_inequalities for key in d]:
-                  if k not in all_inequalities_new_dict: 
-                          all_inequalities_new_dict[k]=[v]
-                  else: 
-                      all_inequalities_new_dict[k].append(v)
-              all_equalities = []
-              for i in range(len(output)):
-                  all_equalities.append(output[i][2])
-              all_equalities_new_dict={}
-              for k,v in [(key,d[key]) for d in all_equalities for key in d]:
-                  if k not in all_equalities_new_dict: 
-                          all_equalities_new_dict[k]=[v]
-                  else: 
-                      all_equalities_new_dict[k].append(v)
-              sql_query = build_string(output[0][0],output[0][1],all_equalities_new_dict,output[0][3],all_inequalities_new_dict,output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
+        ## When there are different criteria in both equalities and inequalities => or in both equalities and inequalities
+        elif ~all(x == equalities[0] for x in equalities) and ~all(x == inequalities[0] for x in inequalities):
+            all_inequalities = []
+            for i in range(len(output)):
+                all_inequalities.append(output[i][4])
+            all_inequalities_new_dict={}
+            for k,v in [(key,d[key]) for d in all_inequalities for key in d]:
+                if k not in all_inequalities_new_dict: 
+                        all_inequalities_new_dict[k]=[v]
+                else: 
+                    all_inequalities_new_dict[k].append(v)
+            all_equalities = []
+            for i in range(len(output)):
+                all_equalities.append(output[i][2])
+            all_equalities_new_dict={}
+            for k,v in [(key,d[key]) for d in all_equalities for key in d]:
+                if k not in all_equalities_new_dict: 
+                        all_equalities_new_dict[k]=[v]
+                else: 
+                    all_equalities_new_dict[k].append(v)
+            sql_query = self.build_string(output[0][0],output[0][1],all_equalities_new_dict,output[0][3],all_inequalities_new_dict,output[0][5],output[0][6],output[0][7],output[0][8],output[0][9],output[0][10])
 
-      ## For non OR query
-      else:
-          left = datalog_str.split(separator )[0]
-          right = datalog_str.split(separator )[1]
-          output = self.dl_parse(left,right)
-          sql_query = self.build_string(output[0],output[1],output[2],output[3],output[4],output[5],output[6],output[7],output[8],output[9],output[10])
+    ## For non OR query
+    else:
+        left = datalog_str.split(separator )[0]
+        right = datalog_str.split(separator )[1]
+        output = self.dl_parse(left,right)
+        sql_query = self.build_string(output[0],output[1],output[2],output[3],output[4],output[5],output[6],output[7],output[8],output[9],output[10])
         
-      return sql_query
+    return sql_query
   
   def parse(self, datalog_str):
     """input is datalog string & output is sql string"""
